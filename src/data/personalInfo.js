b7306254c7558b37a9df50b3d44a647aaf4ea674
Prudhvi Sairam Chundru <chunp01@pfw.edu>
2025-03-27 22:07:44 -0400

    Fix resume download path

--- src/data/personalInfo.js
+++ src/data/personalInfo.js
@@ -7,7 +7,7 @@ export const personalInfo = {
   linkedinUrl: "https://linkedin.com/in/prudhvisairam19",
   github: "Prudhvi-19",
   githubUrl: "https://github.com/Prudhvi-19",
-  resumeFile: "/files/Prudhvi_Sairam_Chundru_Resume.pdf",
+  resumeFile: "/portfolio/files/Prudhvi_Sairam_Chundru_Resume.pdf",
   summary: "I am a Senior Applications Developer with over 3 years of experience at Oracle, where I worked in the Payments team of Fusion Financials. My expertise lies in developing and enhancing enterprise-level financial applications using Java, SQL, PL/SQL, Python, and JavaScript.",
   additionalInfo: [
     "Currently, I am pursuing a Master's in Computer Science at Purdue University Fort Wayne, expanding my knowledge in areas such as Software Engineering, Natural Language Processing, and Quantum Computing.",
